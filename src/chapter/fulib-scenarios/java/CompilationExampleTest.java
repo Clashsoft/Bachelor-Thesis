a81843309ea6fdb75c49b07971f04c5d17f06229
Adrian Kunz <clashsoft@hotmail.com>
2020-03-01 11:17:03 +0100

    Add generated code for compilation example

---
+++ src/chapter/fulib-scenarios/java/CompilationExampleTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.junit.Test;
+
+public class CompilationExampleTest {
+	@Test
+	public void myScenario() {
+		Student alice = new Student();
+		alice.setName("Alice");
+	}
+}
